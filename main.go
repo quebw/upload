d426ab5c7d4f18d417c2bb1c9888f714bcabe8f6
P.Phương <65638913+quebw@users.noreply.github.com>
2022-02-24 15:07:55 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,114 +1,158 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"context"
 	"log"
-	"sort"
-	"strings"
+	"strconv"
+	"time"
 )
 
-type User struct {
-	name    string
-	age     int64
-	gender  bool
-	address string
-}
-
-func (u *User) GetName() string {
-	return u.name
-}
-func (u *User) GetAge() int64 {
-	return u.age
-}
-func (u *User) GetGender() bool {
-	return u.gender
-}
-func (u *User) GetAddress() string {
-	return u.address
-}
-
-type Serve struct {
-	Name  string `json:name`
-	Class string `json:class`
-}
-
-var serves []*Serve
-
-func ex2() {
-	file, err := ioutil.ReadFile("serve.json")
-	if err != nil {
-		log.Println(err)
+func ex_1() {
+	for i := 1; i <= 3; i++ {
+		log.Println("time now: {milliseconds}")
+		time.Sleep(3 * time.Second)
 	}
-	err = json.Unmarshal(file, &serves)
-	if err != nil {
-		log.Println(err)
-	}
-	substr := "Admin"
-	for i, v := range serves {
-		log.Println(i, v)
+	log.Println("ket thuc")
+}
 
-		check_1 := strings.Contains(v.Name, substr)
-		check_2 := strings.Contains(v.Class, substr)
-		if check_1 == true || check_2 == true {
-			log.Println(substr,v)
-		} else {
-			log.Println("Cannot found")
+func ex_2() {
+	timestamp := time.Now().Unix()
+	log.Println("Unix timestamp is:", timestamp)
+	now := time.Now()
+	log.Println("Current timestamp is:", now)
+}
+
+func ex_3(ctx context.Context) error {
+	ctx, cancle := context.WithTimeout(ctx, 3*time.Second)
+	defer cancle()
+	for i := 0; i <= 3; i++ {
+		select {
+		case <-ctx.Done():
+			log.Println("Time out!")
+			return ctx.Err()
+		default:
+			time.Sleep(3 * time.Second)
+			log.Println("No :", i)
 		}
 	}
-
+	log.Println("All done")
+	return nil
 }
 
-// func ex3() {
-
-// }
-
-func ex4() []*Serve {
-	input := Serve{Name: "fileCustome", Class: "org.cofax.cds.FileServlet.Custome"}
-	new := append(serves, &input)
-	return new
-}
-
-func ex6() {
-	slice := []int{11, 34, 56, 77, 99, 109, 66, 20, 88, 34}
-	log.Println(slice)
-	sort.Ints(slice)
-	log.Println(slice)
-	slice1 := make([]int, 7)
-	copy_1 := copy(slice1, slice)
-	log.Println(slice1, copy_1)
-	copy_2 := copy(slice, slice[1:15])
-	log.Println(copy_2)
-}
-
-func ex7() {
-	// users := User{}
-	u1 := &User{name: "A", age: 20, gender: false, address: "abc"}
-	u2 := &User{name: "B", age: 21, gender: true, address: "abc"}
-	u3 := &User{name: "C", age: 20, gender: false, address: "abc"}
-	u4 := &User{name: "D", age: 21, gender: true, address: "abc"}
-
-	new := map[string]*User{u1.GetName(): u1, u2.GetName(): u2, u3.GetName(): u3, u4.GetName(): u4}
-	for i, value := range new {
-		log.Println(i, value)
+func ex_4() {
+	unixTimestamp := "1592190294764144364"
+	unixIntValue, err := strconv.ParseInt(unixTimestamp, 10, 64)
+	if err != nil {
+		log.Println(err)
 	}
+	timeStamp := time.Unix(unixIntValue, 0)
+	// log.Println(timeStamp)
+	hr, min, sec := timeStamp.Clock()
+	log.Printf("Clock: [%d]hour : [%d]minutes : [%d]second", hr, min, sec)
+	log.Println("number_of_minutes of unixTimestamp :", hr*60+min+sec/60)
+
+}
+
+func ex_5() {
+	unixTimestamp := "1592190385"
+	unixIntValue, err := strconv.ParseInt(unixTimestamp, 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+	newTime := time.Unix(unixIntValue, 0)
+	strDate := newTime.Format(time.UnixDate)
+	log.Println("Number :", newTime.UTC())
+	log.Println("String :", strDate)
+}
+
+func x(ctx context.Context) {
+	time_now := time.Now().UnixNano()
+
+	select {
+	case <-time.After(3 * time.Second):
+		now := time.Now().UnixNano() - time_now
+		log.Println("Result:", now)
+	case <-ctx.Done():
+		log.Println(ctx.Err())
+	}
+}
+
+func ex_7() {
+	// time_now := time.Now().UnixNano()
+
+	// ctx, cancel := context.WithCancel(context.Background())
+	// select {
+	// case <-time.After(3 * time.Second):
+	// 	now := time.Now().UnixNano() - time_now
+	// 	log.Println("Result:", now)
+	// case <-ctx.Done():
+	// 	log.Println(ctx.Err())
+	// }
+	// cancel()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	x(ctx)
+	cancel()
+}
+
+func ex_8() {
+	ticker := time.NewTicker(100 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				log.Println("${time.Now().Unix()} done", t)
+			}
+		}
+	}()
+
+	time.Sleep(500 * time.Millisecond)
+	ticker.Stop()
+	done <- true
+	log.Println("Done!")
+
+}
+
+func ex_9() {
+	required_Time := time.Duration(100) * time.Millisecond
+	// log.Println(required_Time)
+	f := func() {
+		log.Println("im study")
+	}
+	Timer1 := time.AfterFunc(required_Time, f)
+	defer Timer1.Stop()
+	time.Sleep(2 * time.Second)
 }
 
 func main() {
-	ex2()
-	// log.Println("------------------Ex3--------------------")
-	// ex3()
-	log.Println("------------------Ex4--------------------")
-	input := ex4()
-	for i, v := range input {
-		log.Println(i, v)
+	log.Println("----------------------Ex1----------------------")
+	ex_1()
+	log.Println("----------------------Ex2----------------------")
+	ex_2()
+	log.Println("----------------------Ex3----------------------")
+	ctx := context.Background()
+	err := ex_3(ctx)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Success!")
 	}
-	log.Println("------------------Ex5--------------------")
-	address := ex4()
-	log.Println(address)
-	log.Println("------------------Ex7--------------------")
-	ex7()
-	log.Println("------------------Ex6--------------------")
-	ex6()
+	log.Println("----------------------Ex4----------------------")
+	ex_4()
+	log.Println("----------------------Ex5----------------------")
+	ex_5()
+	log.Println("----------------------Ex6----------------------")
+	log.Println("Cac moc don vi: yyyy-mm-dd hh:mm:ss + nsec nanoseconds")
+	log.Println("----------------------Ex7----------------------")
+	ex_7()
+	log.Println("----------------------Ex8----------------------")
+	ex_8()
+	log.Println("----------------------Ex9----------------------")
+	ex_9()
 
 }
